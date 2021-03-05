056c84b9f9b435b3eec4b9375238a5f723a709f3
maksimvoevodskij81 <maksimvoevodskij81@gmail.com>
2021-03-05 18:18:55 +0200

    init: task towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  try{
+        if(matrix.length==0){
+             return [];
+            }
+            let flattend = [];
+            (function flat(matrix){
+              for (let i = 0; i < matrix.length; i++) {
+                if(i%2!=0){
+                  matrix[i].reverse();
+                }
+                  flattend.push(matrix[i]);
+              }
+            })(matrix);
+            return flattend.flat();
+  
+}catch{
+    return [];
+  }
 }
