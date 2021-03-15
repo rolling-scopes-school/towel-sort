c70cebd097442383fef6cb080211acc448fb1d58
geowashto <63078138+geowashto@users.noreply.github.com>
2021-03-15 13:47:36 +0200

    Towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
 // You should implement your task here.
+module.exports = function towelSort(matrix) {
 
-module.exports = function towelSort (matrix) {
-  return [];
+    let myArray = [];
+
+    if (Array.isArray(matrix) && matrix.length > 0) {
+
+        for (i = 0; i < matrix.length; i++) {
+
+            if (i % 2 !== 0) {
+                matrix[i] = matrix[i].reverse();
+            }
+
+            let elements = matrix[i];
+
+            for (n = 0; n < elements.length; n++) {
+                myArray.push(elements[n]);
+            }
+        }
+    }
+
+    return myArray;
 }
